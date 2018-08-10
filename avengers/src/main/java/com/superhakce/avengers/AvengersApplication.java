4fe728553383ef1bd3b1510d9b84512d5ac39873
heqingjiang <heqingjiang@ucfgroup.com>
2018-08-10 11:58:02 +0800

    Eureka 配置优化

--- avengers/src/main/java/com/superhakce/avengers/AvengersApplication.java
+++ avengers/src/main/java/com/superhakce/avengers/AvengersApplication.java
@@ -2,8 +2,12 @@ package com.superhakce.avengers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableEurekaServer
+@PropertySource({"classpath:application.properties", "classpath:application.yml"})
 public class AvengersApplication {
 
     public static void main(String[] args) {
