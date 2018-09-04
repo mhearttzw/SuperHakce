709190c6b643a20fd9bd9f0a1a5a2de7c3c1b2ea
heqingjiang <heqingjiang@ucfgroup.com>
2018-09-04 18:59:04 +0800

    测试类

---
+++ algorithm/src/main/java/com/superhakce/algorithm/reactor/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @Author: heqingjiang
+ * @Maintenance: author
+ * @Description: reactor 测试
+ * @Date: Create in 2018/8/31 15:31
+ */
+package com.superhakce.algorithm.reactor;
---
+++ algorithm/src/test/java/com/superhakce/algorithm/DistributedAffairsTest.java
@@ -0,0 +1,27 @@
+package com.superhakce.algorithm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author: heqingjiang
+ * @Maintenance: author
+ * @Description: 分布式事务，分布式锁
+ * @Date: Create in 2018/9/4 18:39
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class DistributedAffairsTest {
+    /**
+     * -----------------------------------------------------------
+     * 纯粹手写TCC
+     *
+     * tcc-transaction框架实现分布式框架
+     * LCN框架实现分布式事务
+     * redis实现分布式锁
+     * -----------------------------------------------------------
+     */
+}
