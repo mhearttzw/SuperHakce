f443fa9984397f23c5a4cad3fad951c91e0df2d8
heqingjiang <heqingjiang@ucfgroup.com>
2018-09-25 19:38:05 +0800

    测试类

--- algorithm/src/test/java/com/superhakce/algorithm/DistributedAffairsTest.java
+++ algorithm/src/test/java/com/superhakce/algorithm/DistributedAffairsTest.java
@@ -18,7 +18,6 @@ public class DistributedAffairsTest {
     /**
      * -----------------------------------------------------------
      * 纯粹手写TCC
-     *
      * tcc-transaction框架实现分布式框架
      * LCN框架实现分布式事务
      * redis实现分布式锁
